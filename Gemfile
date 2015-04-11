b72172ba33855d37a844205129628d683cda5f3c
Sergey Suprunenko <suprunenko.s@gmail.com>
2015-04-11 22:21:06 +0300

    [feature] Shutterstock API calls

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'sinatra'
 gem 'omniauth-shutterstock-contributor', '0.1.1'
 gem 'dotenv'
+gem 'httparty'
 
 group :development do
   gem 'pry'
--- shutterstock.rb
+++ shutterstock.rb
@@ -3,6 +3,7 @@ require 'sinatra'
 require 'omniauth-shutterstock-contributor'
 require 'dotenv'
 require 'pry'
+require 'httparty'
 
 Dotenv.load
 
@@ -18,18 +19,55 @@ end
 
 get '/auth/:provider/callback' do
   auth = request.env['omniauth.auth']
-  session[:name] = auth.info.name
-  session[:email] = auth.info.email
+  session[:access_token] = auth.credentials.token
+  session[:contributor_id] = auth.info.contributor_id
 
   redirect :profile
 end
 
 get '/profile' do
+  client = Shutterstock.new(session[:access_token], session[:contributor_id])
+  user = client.user
   html =
   """
     <h1>Shutterstock Contributor Profile</h1>
-    <h2>#{session[:name]}</h2>
-    <a href='mailto:#{session[:email]}'>#{session[:email]}</a>
+    <h2>#{user['full_name']}</h2>
+    <a href='mailto:#{user['email']}'>#{user['email']}</a>
   """
   html
+end
+
+get '/earnings' do
+  client = Shutterstock.new(session[:access_token], session[:contributor_id])
+  earnings = client.earnings
+  "<p>#{earnings}</p>"
+end
+
+class Shutterstock
+  include HTTParty
+  base_uri 'https://api.shutterstock.com/v2'
+
+  def initialize(access_token, contributor_id=nil)
+    @options = {
+      headers: {
+        'Content-Type'  => 'application/json',
+        'User-Agent'    => 'Shutterstock Contributor Client',
+        'Authorization' => "Bearer #{access_token}"
+      }
+    }
+
+    @contributor_id = contributor_id
+  end
+
+  def user
+    self.class.get('/user', @options)
+  end
+
+  def profile(id=@contributor_id)
+    self.class.get('/contributors/' + id.to_s, @options)
+  end
+
+  def earnings
+    self.class.get('/contributor/earnings', @options)
+  end
 end
